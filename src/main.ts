22936eac8556cc8ff287210285a6e746e18cc336
Fawaz Adebayo <lordrisk101@gmail.com>
2023-01-29 12:40:09 +0100

    latest

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,18 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
-import { PrismaService } from './prisma/prisma.service';
-import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
+import { PrismaService } from './modules/prisma/prisma.service';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  app.useGlobalPipes(new ValidationPipe({transform: true, whitelist: true}));
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalInterceptors(
     new ClassSerializerInterceptor(app.get(Reflector), {
-      strategy: 'excludeAll'
-  })
-  )
-
+      strategy: 'excludeAll',
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('TEAM PSYCHE')
